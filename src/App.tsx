b91e7e719d79be61f037676a9598eb34fdf72330
ramonmuffler <ramon.muffler@gmail.com>
2025-07-14 11:19:16 +0200

    Home glitch fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Game from "./Game";
 const App: React.FC = () => {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
       <Route path="/game" element={<Game />} />
       <Route path="*" element={<Navigate to="/home" replace />} />
