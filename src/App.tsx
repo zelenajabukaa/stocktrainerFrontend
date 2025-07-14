523517836919c0a3caf2cbfcd33e6cdb3561105c
ramonmuffler <ramon.muffler@gmail.com>
2025-07-14 15:29:48 +0200

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import './App.css';
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Home";
 import Game from "./Game.tsx";
+import Header from "./components/Header.tsx";
 
 const App: React.FC = () => {
   return (
@@ -9,6 +11,7 @@ const App: React.FC = () => {
       <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
       <Route path="/game" element={<Game />} />
+      <Route path="/header" element={<Header />} />
       <Route path="*" element={<Navigate to="/home" replace />} />
     </Routes>
   );
