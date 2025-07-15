d6b867fba4e5378ee0381918cc31edfe058cd203
Joris Karpf <joris.karpf@gmail.com>
2025-07-15 14:04:04 +0200

    quests

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,18 @@ import Game from "./Game.tsx";
 import Login from "./components/Login.tsx";
 import PrivateRoute from "./components/PrivateRoute"; // ← NEU
 import Weekly from "./Weekly.tsx";
+import Quests from "./components/Quests.tsx";
 
 const App: React.FC = () => {
   return (
     <Routes>
+      <Route path="/test/quests" element={<Quests />} />
       <Route path="/game/monthly" element={<Game />} />
       <Route path="/game/weekly" element={<Weekly />} />
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
       <Route path="/game" element={<PrivateRoute><Game /></PrivateRoute>} />
+      <Route path="/quests" element={<PrivateRoute><Quests /></PrivateRoute>} />
     </Routes>
   );
 };
